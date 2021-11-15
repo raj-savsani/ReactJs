c51f1b03f9949ed4d35e130e37f5216375786c38
raj-savsani <rrsavsani@gmail.com>
2021-11-15 22:01:48 +0530

    onclick event basic with this.state function

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,11 +3,17 @@ import React, { Component } from "react";
 import "./App.css";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
+import Message from "./components/Message";
+import Counter from "./components/Counter";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Counter/>
+        
+        {/* <Message/> */}
+{/*         
         <Greet name="Raj" heroName="Batman">
           <p>This is Children porps</p>
         </Greet>
@@ -18,7 +24,10 @@ class App extends Component {
         
         <Welcome name="Raj" heroName="Batman"></Welcome>
         <Welcome name="Yagnik" heroName="Superman"></Welcome>
-        <Welcome name="Chirag" heroName="Ironman"></Welcome>
+        <Welcome name="Chirag" heroName="Ironman"></Welcome> 
+*/}
+
+
       </div>
     );
   }
---
+++ hello-world/src/components/Counter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class counter extends Component {
+    render() {
+        return (
+            <div>
+                count 
+            </div>
+        )
+    }
+}
+
+export default counter
