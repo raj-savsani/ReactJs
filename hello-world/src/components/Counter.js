a2601ac427cfecb4ab896765dbdb97ae52f84e4f
raj-savsani <rrsavsani@gmail.com>
2021-11-15 22:45:19 +0530

    couter with set State method

--- hello-world/src/components/Counter.js
+++ hello-world/src/components/Counter.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
- class counter extends Component {
-    render() {
-        return (
-            <div>
-                count 
-            </div>
-        )
-    }
+class counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+  Inc() {
+      this.setState({
+          count: this.state.count+1
+      },()=>{ console.log("callback Value",this.state.count)})
+      console.log('this.state.count:', this.state.count)
+  }
+
+  render() {
+    return (
+      <div>
+        <div>count - {this.state.count}</div>
+        <button onClick={() => this.Inc()}>Increment</button>
+      </div>
+    );
+  }
 }
 
-export default counter
+export default counter;
