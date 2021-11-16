d597aa9c28e7b0e9cd6b3402400c98ccc61780c4
raj-savsani <rrsavsani@gmail.com>
2021-11-17 00:13:02 +0530

    4 diff ways to bind event handlers in react

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -6,13 +6,19 @@ import Welcome from "./components/Welcome";
 import Message from "./components/Message";
 import Counter from "./components/Counter";
 import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
+import EventBind from "./components/EventBind";
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FunctionClick/>
+
+        <EventBind></EventBind>
+        {/* <FunctionClick/> */}
+        {/* <ClassClick/> */}
 
         {/* <Counter/> */}
 
