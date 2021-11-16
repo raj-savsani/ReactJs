b5792ee3ea9e9f0f422b60b566245846074b139f
raj-savsani <rrsavsani@gmail.com>
2021-11-16 23:30:00 +0530

    click event handler in fuctional components

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -5,25 +5,29 @@ import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Message from "./components/Message";
 import Counter from "./components/Counter";
+import FunctionClick from "./components/FunctionClick";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <FunctionClick/>
+
         {/* <Counter/> */}
 
         {/* <Message/> */}
         
-        <Greet name="Raj" heroName="Batman">
+        {/* <Greet name="Raj" heroName="Batman">
           <p>This is Children porps</p>
-        </Greet>
+        </Greet> */}
 
         {/* <Greet name="Yagnik" heroName="Superman">
           <button>Action</button>
         </Greet>
         <Greet name="Chirag" heroName="Ironman" /> */}
         
-        <Welcome name="Raj" heroName="Batman"></Welcome>
+        {/* <Welcome name="Raj" heroName="Batman"></Welcome> */}
         {/* <Welcome name="Yagnik" heroName="Superman"></Welcome>
         <Welcome name="Chirag" heroName="Ironman"></Welcome>  */}
 
