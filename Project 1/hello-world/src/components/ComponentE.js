cb5a8569ebb7e4bc09798c5393a14580f48ce014
raj-savsani <rrsavsani@gmail.com>
2021-12-17 16:07:21 +0530

    Revert "Revert "context type Property""
    This reverts commit 96c7d95329eb104f9d4b8f8c306d758bf372f264.

--- Project 1/hello-world/src/components/ComponentE.js
+++ Project 1/hello-world/src/components/ComponentE.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react'
-import ComponentF from './ComponentF'
+import React, { Component } from "react";
+import ComponentF from "./ComponentF";
+import UserContext from "./UserContext";
 
 class ComponentE extends Component {
-    render() {
-        return (
-            <div>
-                <ComponentF></ComponentF>
-            </div>
-        )
-    }
+static contextType = UserContext
+  render() {
+    return (
+      <div>
+        Component E context {this.context}
+        <ComponentF></ComponentF>
+      </div>
+    );
+  }
 }
 
-export default ComponentE
+// ComponentE.contextType = UserContext;
+export default ComponentE;
--- Project 1/hello-world/src/components/UserContext.js
+++ Project 1/hello-world/src/components/UserContext.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 
-const UserContext = React.createContext()
+const UserContext = React.createContext('Raj Sav')
 
 const UserProvider = UserContext.Provider
 
 const UserConsumer = UserContext.Consumer
 
 export {UserProvider, UserConsumer} 
+export default UserContext
