4da831939bdf958fa2c7a3fffb3e9bef42893c77
raj-savsani <rrsavsani@gmail.com>
2021-12-18 22:10:44 +0530

    using useEffect
    it same as class components lifecycle method Componentsdidmount.
    & componentwillmountandUpdate

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
 import HookCounterFour from './components/HookCounterFour';
+import HookCounterOne from './components/HookCounterOne';
 import HookCounterThree from './components/HookCounterThree';
 import HookCounterTwo from './components/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterFour></HookCounterFour>
+      <HookCounterOne></HookCounterOne>
+      {/* <HookCounterFour></HookCounterFour> */}
       {/* <HookCounterThree></HookCounterThree> */}
       {/* <HookCounterTwo></HookCounterTwo> */}
       {/* <HookCounter></HookCounter> */}
---
+++ state-hook/src/components/HookCounterOne.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+function HookCounterOne() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Clicked {count} times</button>
+    </div>
+  );
+}
+
+export default HookCounterOne;
