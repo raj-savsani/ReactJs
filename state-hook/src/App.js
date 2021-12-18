101a33ec3965e49cf53d4b44e672d2189881bf2d
raj-savsani <rrsavsani@gmail.com>
2021-12-18 21:43:18 +0530

    useState hooks in array

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
+import HookCounterFour from './components/HookCounterFour';
 import HookCounterThree from './components/HookCounterThree';
 import HookCounterTwo from './components/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterThree></HookCounterThree>
+      <HookCounterFour></HookCounterFour>
+      {/* <HookCounterThree></HookCounterThree> */}
       {/* <HookCounterTwo></HookCounterTwo> */}
       {/* <HookCounter></HookCounter> */}
       {/* <ClassCounter></ClassCounter> */}
