883ed4288b1ee2d2d70a3a993c893db2ed8aedf0
raj-savsani <rrsavsani@gmail.com>
2021-12-19 11:15:16 +0530

    condtinally run UseEffects

--- state-hook/src/components/HookCounterOne.js
+++ state-hook/src/components/HookCounterOne.js
@@ -2,12 +2,19 @@ import React, { useState, useEffect } from "react";
 
 function HookCounterOne() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
   useEffect(() => {
+    console.log("useEffect - Updating document title");
     document.title = `You clicked ${count} times`;
-  });
+  },[count]);
   return (
     <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      ></input>
       <button onClick={() => setCount(count + 1)}>Clicked {count} times</button>
     </div>
   );
