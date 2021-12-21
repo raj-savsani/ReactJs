3187bf7cc27944db225207a7e9a83dbc26d63343
raj-savsani <rrsavsani@gmail.com>
2021-12-21 17:46:31 +0530

    using button click for useEffect
    here we are using button click for set id and then fetching data of that id from api

--- state-hook/src/components/DataFetching.js
+++ state-hook/src/components/DataFetching.js
@@ -4,10 +4,13 @@ import axios from "axios";
 function DataFetching() {
   const [posts, setPosts] = useState({});
   const [id, setId] = useState(1);
-
+  const [idfrombutton, setIdfrombutton] = useState(1);
+  const clickHandler = ()=>{
+    setIdfrombutton(id)
+  }
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${idfrombutton}`)
       .then((res) => {
         console.log(res.data);
         setPosts(res.data);
@@ -15,7 +18,7 @@ function DataFetching() {
       .catch((err) => {
         console.log(err);
       });
-  }, [id]);
+  }, [idfrombutton]);
   return (
     <div>
       <input
@@ -25,6 +28,7 @@ function DataFetching() {
           setId(e.target.value);
         }}
       ></input>
+      <button onClick={clickHandler}>Fetch post</button>
       <ul>
         <li key={posts.id}>{posts.title}</li>
       </ul>
